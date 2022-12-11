43c1c9989a5f8136bf71fa2bb544323e28f4dd18
Aleksandr Shchilkin <crazyredkitten@icloud.com>
2022-12-11 19:20:26 +0200

    feat: add backend-app, which will update database with drone info

--- backend-app/src/index.ts
+++ backend-app/src/index.ts
@@ -1,1 +1,22 @@
-console.log("Test node ts");
+import axios from "axios";
+import { XMLParser } from "fast-xml-parser";
+// TODO: Create monorepo
+import { Drone } from "./types/index";
+
+const parserOptions = {
+  ignorePiTags: true,
+};
+const parser = new XMLParser(parserOptions);
+
+try {
+  (async () => {
+    const data = await axios
+      .get("https://assignments.reaktor.com/birdnest/drones")
+      .then((response) => response.data);
+    parser.parse(data)["report"]["capture"]["drone"].map((drone: Drone) => {
+      console.log(drone);
+    });
+  })();
+} catch (error) {
+  console.error(error);
+}
---
+++ backend-app/src/types/index.ts
@@ -0,0 +1,33 @@
+export interface Drone {
+  serialNumber: string;
+  model: string;
+  manufacturer: string;
+  mac: string;
+  ipv4: string;
+  ipv6: string;
+  firmware: string;
+  positionY: number;
+  positionX: number;
+  altitude: number;
+}
+
+export interface Pilot {
+  pilotId: string;
+  firstName: string;
+  lastName: string;
+  phoneNumber: string;
+  createdDt: string;
+  email: string;
+}
+
+/**
+ Type for the string which represent the date and could be parsed by {@link Date} object
+ **/
+type dateString = string;
+
+export interface Info {
+  listenRange: number;
+  deviceStarted: dateString;
+  uptimeSeconds: number;
+  updateIntervalMs: number;
+}
