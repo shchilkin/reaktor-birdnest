5af930e5cd05489325373a75fcf9c2316f9542ef
Aleksandr Shchilkin <crazyredkitten@icloud.com>
2022-12-12 17:48:30 +0200

    WIP: convert to monorepo

---
+++ packages/backend-app/src/index.ts
@@ -0,0 +1,21 @@
+import axios from "axios";
+import { XMLParser } from "fast-xml-parser";
+import { Drone } from "@reaktor-birdnest/birdnest-shared";
+
+const parserOptions = {
+  ignorePiTags: true,
+};
+const parser = new XMLParser(parserOptions);
+
+try {
+  (async () => {
+    const data = await axios
+      .get("https://assignments.reaktor.com/birdnest/drones")
+      .then((response) => response.data);
+    parser.parse(data)["report"]["capture"]["drone"].map((drone: Drone) => {
+      console.log(drone);
+    });
+  })();
+} catch (error) {
+  console.error(error);
+}
